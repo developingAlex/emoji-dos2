ff54ec57b063c000068a7eb462864ee5e2491e03
Alex Palma <alex-palma@outlook.com>
2017-11-28 12:52:28 +1100

    Change the way we make the change to our apps state to a more functional programming immutative way

--- src/App.js
+++ src/App.js
@@ -17,13 +17,39 @@ class App extends Component {
   onToggleItem = (index) => { //to handle what happens when the user clicks a checkbox
     this.setState((prevState) => {
       //get current items
-      const alteredItems = prevState.items //this is the data we wish to change
-      //find the appropriate item by the index
-      const item = alteredItems[index]
-      item.completed = !item.completed
+      const beforeItems = prevState.items //this is the data we wish to change
+      const afterItems = beforeItems.map((item, currentIndex) => {
+        if (currentIndex === index) { //when we have found the index of the item to change
+          
+          //the old way:
+          //create a copy of the item
+          // const copy = Object.assign(
+          //   {}, //start with a new blank object
+          //   item, //copy state of the existing item (its attributes)
+          //   {completed: !item.completed} //overwrite the completed value to be opposite what it was
+          // )
+
+          //the modern way:
+          const copy = {
+            ...item, 
+            completed: !item.completed
+          }
+
+          //and return it
+          return copy
+        }
+        else{ //return what it was
+          return item
+        }
+      })
+
+      //old original non functional mutative way:
+      // //find the appropriate item by the index
+      // const item = afterItems[index]
+      // item.completed = !item.completed
 
       return { //return the changes we recorded
-        items: alteredItems
+        items: afterItems
       }
     })
   }
